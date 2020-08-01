abcb5ee8e08f40a8d6acb70ef21c0731aabbc505
Febri Wardhana <developerlukuzu@gmail.com>
2020-08-01 17:39:57 +0700

    Fixed Detail Pages API

--- Controllers/apiController.js
+++ Controllers/apiController.js
@@ -81,7 +81,8 @@ module.exports = {
           path: "featureId",
           select: "_id name qty imageUrl",
         })
-        .populate({ path: "activityId", select: "_id name type imageUrl" });
+        .populate({ path: "activityId", select: "_id name type imageUrl" })
+        .populate({ path: "imageId", select: "_id imageUrl" });
 
       const bank = await Bank.find();
 
