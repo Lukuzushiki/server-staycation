68469d198352f408722a021b87bba85942091c8a
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2021-06-15 08:05:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(
 app.use(function (req,res, next) {
   // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'https://staycation-project.herokuapp.com');
+    res.setHeader('Access-Control-Allow-Origin', 'https://staycation-topaz.vercel.app');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
