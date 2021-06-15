c8c99f10d59a1d1d685d12b4a63a48f63f8410e6
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2021-06-15 07:33:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -39,6 +39,23 @@ app.use(
     cookie: { maxAge: 60000 },
   })
 );
+app.use(function (req,res, next) {
+  // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+})
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
