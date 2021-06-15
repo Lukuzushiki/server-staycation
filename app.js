2596f88c6500b2dad2c77d91a6b261620bf688b2
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2021-06-15 08:12:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ const adminRouter = require("./routes/admin");
 const apiRouter = require("./routes/api");
 
 var app = express();
-app.use(cors());
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -40,24 +39,6 @@ app.use(
     cookie: { maxAge: 60000 },
   })
 );
-app.use(function (req,res, next) {
-  // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://staycation-project.herokuapp.com');
-    res.setHeader('Access-Control-Allow-Origin', 'https://staycation-topaz.vercel.app');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-})
 app.use(flash());
 app.use(logger("dev"));
 app.use(express.json());
@@ -69,6 +50,7 @@ app.use(
   express.static(path.join(__dirname, "node_modules/startbootstrap-sb-admin-2"))
 );
 
+app.use(cors());
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 //Admin
