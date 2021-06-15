750ab7a47e472c58965375c7f09604f9d19fd714
Febri Wardhana <44961442+Lukuzushiki@users.noreply.github.com>
2021-06-15 07:59:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.use(
 );
 app.use(function (req,res, next) {
   // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://staycation-project.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Origin', 'https://staycation-project.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
